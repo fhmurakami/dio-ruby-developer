99805bfa44d03e30c25243ba0ef719520a1dca1d
Felipe Murakami <13895820+fhmurakami@users.noreply.github.com>
2024-11-02 20:41:53 -0300

    Add first Ruby project

---
+++ primeiro-projeto.rb
@@ -0,0 +1,10 @@
+print('Digite seu nome: ')
+name = gets.chomp
+
+print('Digite seu sobrenome: ')
+last_name = gets.chomp
+
+print('Digite sua idade: ')
+age = gets.chomp
+
+puts("Olá #{name} #{last_name}, você tem #{age} anos")
